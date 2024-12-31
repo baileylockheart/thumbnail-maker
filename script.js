bbf8ca8ccbc3b37325912230045e3d550dcb164b
Bailey Lockheart <baileylockheart@gmail.com>
2024-12-31 12:40:32 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ function setName(){
     document.getElementById("name").innerHTML=n;
 
     let output = document.getElementById("output");
+
     document.getElementById("name").style.display = "flex"
+
     document.getElementById("input").value="";
 }
+
 function setTitle(){
     let t = document.getElementById("input2").value;
     document.getElementById("title").innerHTML=t;
 
     let output = document.getElementById("output");
+
     document.getElementById("title").style.display = "flex"
+
     document.getElementById("input2").value="";
 }
 
@@ -19,6 +24,8 @@ function setBG(){
     box.style.backgroundImage = 'url(' + input3.value + ')';
 
     let output = document.getElementById("output");
+
     document.getElementById("box").style.display = "block"
+    
     document.getElementById("input3").value="";
 }
