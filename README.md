70d29e62f866f7405246b13349113adccdb2655c
Bailey Lockheart <baileylockheart@gmail.com>
2024-12-31 12:27:27 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# thumbnail-maker
-little tool I made to use on my new yt channel :D
+# Thumbnail Maker
+little tool I made to make thumbnails for my new yt channel :D
+
+![Screenshot](https://file.garden/ZrZSgsrYfQXsO7QH/github/Screenshot%202024-12-31%20at%2012-21-52%20Thumbnail%20Maker.png)
---
+++ script.js
@@ -0,0 +1,25 @@
+function setName(){
+    let n = document.getElementById("input").value;
+    document.getElementById("name").innerHTML=n;
+
+    let output = document.getElementById("output");
+    document.getElementById("name").style.display = "flex"
+    document.getElementById("input").value="";
+}
+function setTitle(){
+    let t = document.getElementById("input2").value;
+    document.getElementById("title").innerHTML=t;
+
+    let output = document.getElementById("output");
+    document.getElementById("title").style.display = "flex"
+    document.getElementById("input2").value="";
+}
+
+function setBG(){
+    let b = document.getElementById("input3").value;
+    box.style.backgroundImage = 'url(' + input3.value + ')';
+
+    let output = document.getElementById("output");
+    document.getElementById("box").style.display = "block"
+    document.getElementById("input3").value="";
+}
